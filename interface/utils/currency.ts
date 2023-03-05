5a485e5f7173c9fc7abd3193135caf9a004e1daa
ferrodri <f.rodriguez.hervias@gmail.com>
2023-03-05 03:45:21 -0700

    fix wei

--- interface/utils/currency.ts
+++ interface/utils/currency.ts
@@ -1,5 +1,5 @@
-import { commify, formatEther } from 'ethers/lib/utils';
 import { BigNumberish } from 'ethers';
+import { commify, formatEther } from 'ethers/lib/utils';
 import { round } from './number';
 const ETH_SYMBOL = 'ETH';
 
@@ -7,7 +7,7 @@ export const convertToEth = (wei: BigNumberish) => {
     const eth = formatEther(wei);
     const rounded = round(eth, 6, false);
     const formattedRaw = commify(rounded);
-    const formatted = `${formattedRaw} ${ETH_SYMBOL}`;
+    const formatted = `${wei} ${ETH_SYMBOL}`;
 
     return {
         raw: eth,
