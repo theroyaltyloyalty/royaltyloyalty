0342cc929f44190757519aae5d538ae440b5bfa4
Sebastian Coronel <scoronelhamilton@gmail.com>
2023-03-05 02:39:13 -0700

    style whitelist modal

--- interface/components/modals/WhitelistModal.tsx
+++ interface/components/modals/WhitelistModal.tsx
@@ -1,7 +1,8 @@
 import Modal from '../Modal';
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction, useState } from 'react';
 import { OwnerExtended } from 'types/types';
 import { RiFileCopyLine as IconCopy } from 'react-icons/ri';
+import Copy from 'components/Copy';
 
 export default function WhitelistModal({
     isOpen,
@@ -19,34 +20,52 @@ export default function WhitelistModal({
             <div className="space-y-6">
                 <h2 className="font-bold text-2xl">Create whitelist</h2>
 
-                <div className="bg-white/5 rounded-md">
-                    <div className="w-full px-4 py-3 border-b border-b-[#2F3137] flex items-center justify-between">
-                        <div className="text-xs uppercase font-bold">
-                            Merkle root
-                        </div>
-                        <div>copy</div>
-                    </div>
-                    <div className="p-4">{merkleRoot}</div>
-                </div>
-
-                <div className="bg-white/5 rounded-md">
-                    <div className="w-full px-4 py-3 border-b border-b-[#2F3137] flex items-center justify-between">
-                        <div className="text-xs uppercase font-bold">
-                            Addresses
-                        </div>
-                        <div>Copy</div>
-                    </div>
-                    <div className="p-4 space-y-2">
-                        {selectedOwners.map((owner) => {
-                            return (
-                                <div key={owner.address}>{owner.address}</div>
-                            );
-                        })}
-                    </div>
-                </div>
-
-                <div></div>
+                <ResultArea title="Merkle root" value={merkleRoot} />
+                <ResultArea
+                    title="Addresses"
+                    value={selectedOwners.map((owner) => owner.address)}
+                />
             </div>
         </Modal>
     );
 }
+
+const ResultArea = ({
+    title,
+    value,
+}: {
+    title: string;
+    value: string | string[];
+}) => {
+    const [isCopied, setIsCopied] = useState(false);
+
+    return (
+        <div className="bg-white/5 rounded-md py-4">
+            <div className="w-full px-4 pb-4 flex text-gray-400 items-center justify-between">
+                <div className="text-xs uppercase font-bold">{title}</div>
+                <div className="text-xs cursor-pointer">
+                    <Copy
+                        text={
+                            typeof value === 'string' ? value : value.join('\n')
+                        }
+                        onCopy={() => setIsCopied(true)}
+                    >
+                        {isCopied ? (
+                            'Copied!'
+                        ) : (
+                            <div className="flex items-center">
+                                <IconCopy className="mr-1 h-4 w-4" />
+                                <span>Copy</span>
+                            </div>
+                        )}
+                    </Copy>
+                </div>
+            </div>
+            <div className="px-4 text-sm space-y-2">
+                {typeof value === 'string'
+                    ? value
+                    : value.map((v) => <div key={v}>{v}</div>)}
+            </div>
+        </div>
+    );
+};
